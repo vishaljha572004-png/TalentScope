4609ab4c67dc23c291863eb578236bd9e7c14077
vishaljha572004-png <vishaljha572004@gmail.com>
2026-08-03 09:32:30 +0530

    update 5

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ dotenv.config()
 const app = express()  
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://talentscope-client.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
