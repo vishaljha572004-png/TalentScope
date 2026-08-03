60ce63633eade8276b8b4d3ebefe9ae730ed9f50
vishaljha572004-png <vishaljha572004@gmail.com>
2026-08-03 08:31:01 +0530

    update

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -15,8 +15,8 @@ export const googleAuth = async (req,res) => {
         let token = await genToken(user._id)
         res.cookie("token" , token , {
             httpOnly:true,
-            secure:false,
-            sameSite:"Lax",
+            secure:true,
+            sameSite:"none",
             maxAge:7 * 24 * 60 * 60 * 1000
         })
 
